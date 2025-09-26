b67593111b58af2e912461de954fa1953375de05
Mars <mars@squirrel.pub>
2025-09-27 01:21:33 +0200

    fix empty ul in toctree

--- theme/stf.js
+++ theme/stf.js
@@ -1,9 +1,8 @@
 
 /* Sphinx fixes */
 
-for(let e of document.querySelectorAll(".sidebar-tree .caption")) {
+for(let e of document.querySelectorAll(".sidebar-tree .caption"))
 	e.setAttribute("aria-level", "2");
-}
 
 document.querySelector(".sidebar-tree li.current-page").setAttribute("aria-selected", "true");
 
@@ -12,9 +11,12 @@ document.querySelector(".sidebar-tree li.current-page").setAttribute("aria-selec
 document.querySelector(".content-icon-container").setAttribute("role", "complementary");
 document.querySelector(".content-icon-container .view-this-page .muted-link").setAttribute("title", "View this page's source"); /* The default "View this page" doesn't quite make sense imho. */
 
+for(let e of document.querySelectorAll(".toc-tree ul"))
+	if(e.getHTML().trimStart() == "")
+		e.remove();
 
 /* STF temp fixes, turn TBD buttons into a AAA error */
 
-let e;
-if(e = document.querySelector(".sd-btn-outline-secondary.ignore")) e.setAttribute("role", "presentation");
+for(let e of document.querySelectorAll(".sd-btn-outline-secondary.ignore"))
+	e.setAttribute("role", "presentation");
 
